d5e86161a053f7f9309920f5e10bb7df9692204e
ThiagoDRangel <info.tec.campos@gmail.com>
2023-02-13 20:32:57 -0300

    Config validations name/email

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,9 +1,15 @@
+import returnHash from '../../helpers/returnHash';
+
 export const LOGIN = 'LOGIN';
 
-export const login = (email, password) => ({
-  type: LOGIN,
-  payload: {
-    email,
-    password,
-  },
-});
+export const login = (name, email) => {
+  const image = `https://www.gravatar.com/avatar/${returnHash(email)}`;
+  return {
+    type: LOGIN,
+    payload: {
+      name,
+      email,
+      image,
+    },
+  };
+};
