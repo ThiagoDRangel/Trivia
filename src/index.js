2e6697e3a4892947aa1340158a164a8bdf652402
ThiagoDRangel <info.tec.campos@gmail.com>
2023-02-13 20:23:17 -0300

    Config hash from md5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,6 +1,9 @@
+import logo from '../../images/logo.jpg';
+
 const INITIAL_STATE = {
   username: '',
   email: '',
+  image: logo,
 };
 
 const user = (state = INITIAL_STATE, action) => {
