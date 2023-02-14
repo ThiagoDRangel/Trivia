3989ccf05249fb590dfe1f617baad5dcab12bba9
ThiagoDRangel <info.tec.campos@gmail.com>
2023-02-14 13:21:00 -0300

    API funcional in game.jsx

---
+++ src/helpers/urls.js
@@ -0,0 +1,1 @@
+export const urlToken = 'https://opentdb.com/api.php?';
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
+import QuestionCard from '../components/QuestionCard';
+import fetchApi from '../helpers/fetchApi';
+import { urlToken } from '../helpers/urls';
 
 class Game extends Component {
+  state = {
+    questions: [],
+  };
+
+  componentDidMount() {
+    this.handleToken();
+  }
+
+  handleToken = async () => {
+    const { history } = this.props;
+    const token = localStorage.getItem('token');
+    const TOKEN_ERROR = 3;
+    const data = await fetchApi(
+      `${urlToken}amount=5&token=${token}`);
+    console.log('data', data);
+  }
   render() {
     return (<h1>Game</h1>);
   }
